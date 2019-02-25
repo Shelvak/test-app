0559862419d09c7d187815012a84e19e84ad75fe
Néstor Coppi <nestorcoppi@gmail.com>
2019-02-25 20:01:16 -0300

    Simplify db config

--- test/integration/some_test.rb
+++ test/integration/some_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SomeTest < ActionDispatch::IntegrationTest
   # require 'bindex'
   test 'hello' do
-    skip "Skipping"
-    assert true
+    # skip "Skipping"
+    # assert true
   end
 end
